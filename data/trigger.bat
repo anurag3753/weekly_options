98ca310e613a612aee28799817b929ffcbc3c920
Anurag Modi <modi.anurag1992@gmail.com>
2024-02-12 17:47:10 +0530

    Make git push changes

--- data/trigger.bat
+++ data/trigger.bat
@@ -8,6 +8,14 @@ start "Options" /B cmd /C "call %scriptName%.bat"
 rem Wait for 3 minutes (180 seconds)
 timeout /t 180
 
+rem Push to git repo
+echo Current directory is: %CD%
+git add output_%scriptName%.csv
+git commit -m "update %scriptName%"
+ssh -T git@github.com
+git remote set-url origin git@github.com:anurag3753/weekly_options.git
+git push
+
 rem Kill all instances of Google Chrome
 taskkill /f /im chrome.exe
 
