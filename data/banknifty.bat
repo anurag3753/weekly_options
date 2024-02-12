dda583cd054ff37ae7db00eb4d5f408ee7ebe21a
Anurag Modi <modi.anurag1992@gmail.com>
2024-02-12 16:24:46 +0530

    Fix minor typo

--- data/banknifty.bat
+++ data/banknifty.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd /d %~dp0
-set PYTHON_ENVIRONMENT=D:\trading\weekly_oprions\venv
+set PYTHON_ENVIRONMENT=D:\trading\weekly_options\venv
 
 "%PYTHON_ENVIRONMENT%\Scripts\activate.bat" & python main.py "BANKNIFTY"
 
--- data/nifty.bat
+++ data/nifty.bat
@@ -1,6 +1,6 @@
 @echo off
 
 cd /d %~dp0
-set PYTHON_ENVIRONMENT=D:\trading\weekly_oprions\venv
+set PYTHON_ENVIRONMENT=D:\trading\weekly_options\venv
 
 "%PYTHON_ENVIRONMENT%\Scripts\activate.bat" & python main.py "NIFTY"
